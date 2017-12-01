9fa12037fcfb02071f2fac88476a42f8e0499f7c
xiaochen yang <xiaochenyung@gmail.com>
2017-12-01 10:25:16 +0800

    add test view

--- YCNotificationCenter/FirstViewController.h
+++ YCNotificationCenter/FirstViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  FirstViewController.h
 //  YCNotificationCenter
 //
 //  Created by xiaochen yang on 2017/12/1.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface FirstViewController : UIViewController
 
 @end
-
---
+++ YCNotificationCenter/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  YCNotificationCenter
+//
+//  Created by xiaochen yang on 2017/12/1.
+//  Copyright © 2017年 xiaochen yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
